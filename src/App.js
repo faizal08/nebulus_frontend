d60e469323631955c6629d685359c08b09238904
Faizal Rahmah <reachfaizal08@gmail.com>
2024-08-28 20:04:51 +0530

    005 FIFTH  COMMIT NEBULUS

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div>Hello faizal</div>
+      <div>Hello ivin</div>
+      <div>Hello fayis</div>
+      <div>Hello Sojin</div>
+    </>
+  );
+}
+
+export default App;
